f3ec2394e3b8d073498e0542c2982309006fb8b9
ezlo-mukhtarove <emilmuhtarov1@gmail.com>
2024-04-12 10:27:02 +0200

    Fix actions run

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,8 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { type BuildOptions } from './types/config'
 
-export function buildPlugins ({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html
     }),
@@ -13,10 +13,17 @@ export function buildPlugins ({ paths }: BuildOptions): webpack.WebpackPluginIns
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ]
+
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false
+      })
+    )
+  }
+
+  return plugins
 }
